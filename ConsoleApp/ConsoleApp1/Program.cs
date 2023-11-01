58ff11d8442dbe1f4e8a31666319e01169920315
Ilian Sharkov <62841691+Li4kataaa@users.noreply.github.com>
2023-11-01 15:54:58 +0200

    Add files via upload

---
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
