5c07f58724c230d260d905bc7972a6b13db847cd
Ilian Sharkov <62841691+Li4kataaa@users.noreply.github.com>
2023-11-01 17:16:25 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,12 +17,12 @@ pipeline {
         stage('Restore NuGet For Solution') {
             steps {
                 //  '--no-cache' to avoid a shared cache--if multiple projects are running NuGet restore, they can collide.
-                bat "dotnet restore --nologo --no-cache"
+                //bat "dotnet restore --nologo --no-cache"
             }
         }
         stage('Build Solution') {
             steps {
-                bat "dotnet build --nologo -c Release -p:ProductVersion=1.0.${env.BUILD_NUMBER}.0 --no-restore"
+                //bat "dotnet build --nologo -c Release -p:ProductVersion=1.0.${env.BUILD_NUMBER}.0 --no-restore"
             }
         }
 		stage('Unit Test Stage') {
